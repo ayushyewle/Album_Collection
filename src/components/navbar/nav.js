29229c06aff0920d1a4d082c971378985f93cb1d
ayushyewle <100531576+ayushyewle@users.noreply.github.com>
2024-01-21 23:04:30 +0530

    Update nav.js

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <nav className="container">
-   <Link to ='/' className='logo-link'><h1>Album App</h1></Link>
+   <Link to ='/' className='logo-link'><h1>Album List</h1></Link>
   </nav>
   )
 }
